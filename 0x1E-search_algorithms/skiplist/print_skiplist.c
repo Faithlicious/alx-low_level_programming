cf25a7cbb0ad314f1cf8732d08bca46db14e306a
Faithlicious <violetshiko15@gmail.com>
2024-04-10 18:03:29 -0700

    0x1E-search_algorithms

--- 0x1E-search_algorithms/skiplist/print_skiplist.c
+++ 0x1E-search_algorithms/skiplist/print_skiplist.c
@@ -3,25 +3,25 @@
 #include "search_algos.h"
 
 /**
- *  * print_skiplist - dump the content of a skiplist_t
- *   *
- *    * @list: Pointer to the head of the list
- *     *
- *      * Return: void
- *       */
+ * print_skiplist - dump the content of a skiplist_t
+ *
+ * @list: Pointer to the head of the list
+ *
+ * Return: void
+ */
 void print_skiplist(const skiplist_t *list)
 {
-		const skiplist_t *node;
+	const skiplist_t *node;
 
-			printf("List :\n");
-				for (node = list; node; node = node->next)
-						{
-									printf("Index[%lu] = [%d]\n", node->index, node->n);
-										}
-					printf("\nExpress lane :\n");
-						for (node = list; node; node = node->express)
-								{
-											printf("Index[%lu] = [%d]\n", node->index, node->n);
-												}
-							printf("\n");
+	printf("List :\n");
+	for (node = list; node; node = node->next)
+	{
+		printf("Index[%lu] = [%d]\n", node->index, node->n);
+	}
+	printf("\nExpress lane :\n");
+	for (node = list; node; node = node->express)
+	{
+		printf("Index[%lu] = [%d]\n", node->index, node->n);
+	}
+	printf("\n");
 }
